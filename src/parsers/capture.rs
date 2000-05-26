3daad0a1277788332b8b5c69186c2df5f48f618e
Floaterest <floaterest@outlook.jp>
2000-05-26 10:15:20 -0400

    feat(*.rs) `|a|` to pop capture `a`

--- src/parsers/capture.rs
+++ src/parsers/capture.rs
@@ -5,22 +5,30 @@ use crate::stores::capture::Capture;
 use crate::stores::note::Chord;
 
 const CAPTURE: u8 = b'(';
+const POP: u8 = b'|';
 const SHIFT: u8 = b'[';
 const CLEAR: u8 = b'{';
 const RAISE: u8 = b'^';
 const LOWER: u8 = b'_';
 
 #[derive(PartialEq)]
+pub enum CapOpt {
+    Pop,
+    Shift,
+    CLEAR,
+}
+
+#[derive(PartialEq)]
 pub enum Cap {
     /// (key)
     Capture(String),
-    /// (key, clear, scale)
-    Shift(String, bool, Option<f64>),
+    /// (key, pop|shift|clear, scale)
+    Get(String, CapOpt, Option<f64>),
 }
 
 /// check if a line should be parsed as capture based on the first byte
 pub fn should_be_cap(byte: u8) -> bool {
-    matches!(byte, CAPTURE | SHIFT | CLEAR)
+    matches!(byte, CAPTURE | SHIFT | POP | CLEAR)
 }
 
 /// check if capture is cap
@@ -46,8 +54,9 @@ fn panic_empty(key: &str, action: &str) -> ! {
 fn parse_cap(token: &str, prefix: u8) -> Cap {
     // get matching pair
     let suffix = match prefix {
-        CAPTURE => b')',
-        SHIFT | CLEAR => prefix + 2,
+        b'(' => b')',
+        b'|' => b'|',
+        b'[' | b'{' => prefix + 2,
         _ => panic!("Invalid token as capture: {}", token),
     };
     let bytes = token.as_bytes();
@@ -55,7 +64,9 @@ fn parse_cap(token: &str, prefix: u8) -> Cap {
     let key = token.chars().filter(|ch| ch.is_ascii_alphabetic()).collect();
     match prefix {
         CAPTURE => Cap::Capture(key),
-        SHIFT | CLEAR => Cap::Shift(key, prefix == CLEAR, parse_scale(bytes)),
+        POP => Cap::Get(key, CapOpt::Pop, parse_scale(bytes)),
+        SHIFT => Cap::Get(key, CapOpt::Shift, parse_scale(bytes)),
+        CLEAR => Cap::Get(key, CapOpt::CLEAR, parse_scale(bytes)),
         _ => panic!("wot"),
     }
 }
@@ -75,10 +86,10 @@ pub struct CaptureParser {
     captures: HashMap<Rc<String>, Capture>,
     /// set of keys (rc pointing to keys in captures) to capture
     to_capture: HashSet<Rc<String>>,
-    /// set of keys (rc pointing to keys in captures) to shift
-    to_shift: HashSet<Rc<String>>,
-    /// set of keys (rc pointing to keys in captures) to pop
-    to_pop: HashSet<Rc<String>>,
+    /// set of keys (rc pointing to keys in captures) to shift/pop
+    to_shift: HashSet<(Rc<String>, bool)>,
+    /// set of keys (rc pointing to keys in captures) to clear
+    to_clear: HashSet<Rc<String>>,
 }
 
 impl CaptureParser {
@@ -87,7 +98,7 @@ impl CaptureParser {
             captures: HashMap::new(),
             to_capture: HashSet::new(),
             to_shift: HashSet::new(),
-            to_pop: HashSet::new(),
+            to_clear: HashSet::new(),
         }
     }
     /// schedule key to be captured
@@ -95,13 +106,17 @@ impl CaptureParser {
         self.to_capture.insert(key);
     }
     /// schedule key to shift (or clear) and return current chord in key
-    pub fn will_shift(&mut self, key: Rc<String>, pop: bool) -> Rc<Chord> {
-        let set = if pop { &mut self.to_pop } else { &mut self.to_shift };
+    pub fn will_shift(&mut self, key: Rc<String>, opt: &CapOpt) -> Rc<Chord> {
         match self.captures.get_key_value(&key) {
             Some((key, chord)) => {
                 // insert the key from captures, not from parameters
                 // (they are the same value but rc points to different places)
-                (*set).insert(Rc::clone(key));
+                let rc = Rc::clone(key);
+                match opt {
+                    CapOpt::Pop => self.to_shift.insert((rc, true)),
+                    CapOpt::Shift => self.to_shift.insert((rc, false)),
+                    CapOpt::CLEAR => self.to_clear.insert(rc),
+                };
                 match chord.current() {
                     Some(chord) => chord,
                     None => panic_empty(&key, "get current"),
@@ -119,16 +134,19 @@ impl CaptureParser {
     /// update cycle and clear all HashSets
     pub fn update(&mut self) {
         // assume to_capture is already cleared from self.capture()
-        for shift in self.to_shift.iter() {
+        for (shift, pop) in self.to_shift.iter() {
             match self.captures.get_mut(shift) {
-                Some(chord) => chord.shift(
-                    self.to_pop.contains(shift)
-                ).unwrap_or_else(|| panic_empty(shift, "shift")),
+                Some(chord) => chord.shift(*pop).unwrap_or_else(
+                    || panic_empty(shift, "shift")
+                ),
                 None => panic_not_found(shift, "shift"),
             }
         }
+        for clear in self.to_clear.drain() {
+            self.captures.remove(&clear);
+        }
         self.to_shift.clear();
-        self.to_pop.clear();
+        self.to_clear.clear();
     }
     /// parse token as capture
     pub fn parse(&self, token: &str) -> Option<Cap> {
