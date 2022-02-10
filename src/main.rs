32d1bc532aed68a0550fb48363719ea92315d410
floaterest <56704092+floaterest@users.noreply.github.com>
2022-02-09 19:14:26 -0500

    feat(main,note): read note input as `u8` instead of `Vec<char>`

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ use wave::Wave;
 
 fn main() {
     let mut sc = Scanner::default();
-    let n: Vec<char> = sc.next::<String>().chars().collect();
+    let s = sc.next::<String>();
+    let n: &[u8] = s.as_bytes();
     let ampl = sc.next::<f64>();
     let duration = sc.next::<u32>();
     let frame_rate = sc.next::<u32>();
     let fname = sc.next::<String>();
 
     let w = Wave::new(frame_rate);
-    w.write(note::ntof(&n), ampl, duration, &fname).unwrap();
+    w.write(note::ntof(n), ampl, duration, &fname).unwrap();
 }
