a6d1294a19b0dee1bd926fc55e421ad816fa1e2a
Floaterest <floaterest@outlook.jp>
2022-02-10 12:58:20 -0500

    feat(main): read multiple bpm and multiple notes as chord

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-mod note;
+// mod note;
 mod scanner;
-mod wave;
+// mod wave;
 
 use scanner::Scanner;
-use wave::Wave;
+// use wave::Wave;
 
 fn main() {
     let mut sc = Scanner::default();
-    let s = sc.next::<String>();
-    let n: &[u8] = s.as_bytes();
-    let ampl = sc.next::<f64>();
-    let duration = sc.next::<u32>();
-    let frame_rate = sc.next::<u32>();
-    let fname = sc.next::<String>();
-
-    let w = Wave::new(frame_rate);
-    w.write(note::ntof(n), ampl, duration, &fname).unwrap();
+    let ampl: f64 = sc.next();
+    let fps: u32 = sc.next();
+    let nbpm: usize = sc.next();
+    let fname: String = sc.next();
+    dbg!(ampl,fps,fname);
+    for _ in 0..nbpm {
+        let bpm: u8 = sc.next();
+        dbg!(bpm);
+        let nlines: usize = sc.next();
+        for _ in 0..nlines {
+            let mut line = sc.next_line();
+            let beat: f32 = line.pop().unwrap().parse().unwrap();
+            let notes:Vec<&[u8]> = line.iter().map(|n| n.as_bytes()).collect();
+            dbg!(beat,notes);
+        }
+    }
 }
