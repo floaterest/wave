95ef88781050646811afdcceec1830bf71bf84f3
floaterest <56704092+floaterest@users.noreply.github.com>
2022-02-10 13:24:02 -0500

    feat(main,wave): read sequence of chords

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-// mod note;
+mod note;
 mod scanner;
-// mod wave;
+mod wave;
 
+use std::io::Result;
 use scanner::Scanner;
-// use wave::Wave;
+use wave::Wave;
 
-fn main() {
+fn main() -> Result<()> {
     let mut sc = Scanner::default();
     let ampl: f64 = sc.next();
     let fps: u32 = sc.next();
     let nbpm: usize = sc.next();
     let fname: String = sc.next();
-    dbg!(ampl,fps,fname);
+
+    let mut w = Wave::new(fps, ampl, &fname);
+    w.start()?;
     for _ in 0..nbpm {
-        let bpm: u8 = sc.next();
-        dbg!(bpm);
+        let bpm: f32 = sc.next();
         let nlines: usize = sc.next();
         for _ in 0..nlines {
             let mut line = sc.next_line();
             let beat: f32 = line.pop().unwrap().parse().unwrap();
-            let notes:Vec<&[u8]> = line.iter().map(|n| n.as_bytes()).collect();
-            dbg!(beat,notes);
+            let freqs: Vec<f64> = line.iter().map(|n| note::ntof(n.as_bytes())).collect();
+            w.write(&freqs, beat * 60.0 / bpm)?;
         }
     }
+    w.finish()?;
+
+    Ok(())
 }
