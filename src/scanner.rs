f3ba815c2ee9a45b2d53e51804498e9575e1c1b1
floaterest <56704092+floaterest@users.noreply.github.com>
2022-02-13 15:30:25 -0500

    feat(scanner): support comment (//)

--- src/scanner.rs
+++ src/scanner.rs
@@ -22,6 +22,10 @@ impl Scanner {
         loop {
             let mut line = String::new();
             stdin().read_line(&mut line).expect("Scanner: Read line failed");
+            line = String::from(line.trim());
+            if line.starts_with("//") {
+                continue;
+            }
             let args: Vec<String> = line.split_ascii_whitespace().map(String::from).collect();
             if args.len() != 0 {
                 return args;
