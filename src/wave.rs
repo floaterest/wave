23a0f78b1dd21bc505f625d1124550d007951d6f
Floaterest <floaterest@outlook.jp>
2022-02-15 18:07:47 -0500

    feat(wave): self adjust (will tidy this spaghetti later)

--- src/wave.rs
+++ src/wave.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::slice::from_raw_parts;
 use std::mem::transmute;
 use std::f64::consts::PI;
 use std::io::{Result, Seek, SeekFrom, Write};
@@ -12,6 +13,9 @@ pub struct Wave<'a> {
     pub amplitude: f64,
     /// curve function
     pub fx: &'a dyn Fn(f64) -> f64,
+    /// number of frames that are off beat
+    offset: i8,
+    inc: bool,
 }
 
 impl Wave<'_> {
@@ -23,6 +27,8 @@ impl Wave<'_> {
             frame_width: 2,
             nchannels: 1,
             file: File::create(fname).expect("Wave: Create file failed"),
+            offset: 0,
+            inc: false,
         }
     }
 
@@ -58,16 +64,37 @@ impl Wave<'_> {
     pub fn write(&mut self, freqs: &[f64], duration: f32) -> Result<()> {
         let nframes = (duration * self.frame_rate as f32) as u32;
         let n = 2.0 * PI / self.frame_rate as f64;
+        let a = if self.inc { self.amplitude } else { -self.amplitude };
 
-        let mut bytes: Vec<u8> = Vec::with_capacity((nframes * self.frame_width as u32) as usize);
-        (0..nframes).for_each(|i| {
-            let sin = freqs.iter().map(|f| (f * n * i as f64).sin()).sum::<f64>();
-            bytes.extend_from_slice(&unsafe {
-                transmute((self.amplitude * &(self.fx)(i as f64 / nframes as f64) * sin) as i16)
-            } as &[u8; 2]);
-        });
-
-        self.file.write(&bytes)?;
+        let mut sines: Vec<i16> = (1..nframes).map(|i|
+            (a * &(self.fx)(i as f64 / nframes as f64) * freqs.iter().map(|f| (f * n * i as f64).sin()).sum::<f64>()) as i16
+        ).collect();
+        let pos = sines[sines.len() - 1] > 0;
+        self.inc = !pos;
+        let lpos = sines.iter().rposition(|s| (s < &0) == pos).unwrap() as i16;
+        let mut rpos = nframes as i16;
+        loop {
+            let sin = (a * &(self.fx)(rpos as f64 / nframes as f64) * freqs.iter().map(|f| (f * n * rpos as f64).sin()).sum::<f64>()) as i16;
+            if (sin < 0) == pos {
+                break;
+            }
+            sines.push(sin);
+            rpos += 1;
+        }
+        let beat = nframes as i16 + self.offset as i16;
+        // if left is closer than right
+        if beat - lpos < rpos - beat {
+            self.file.write(unsafe {
+                from_raw_parts(sines.as_ptr() as *const u8, (lpos as usize + 1) * 2)
+            })?;
+            self.offset = (lpos - nframes as i16) as i8;
+            self.inc = !self.inc;
+        } else {
+            self.file.write(unsafe {
+                from_raw_parts(sines.as_ptr() as *const u8, sines.len() * 2)
+            })?;
+            self.offset = (rpos - nframes as i16) as i8;
+        }
 
         Ok(())
     }
