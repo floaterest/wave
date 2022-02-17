4adcc2adb73ce377a3426c5f7e654a20c35e7c99
floaterest <56704092+floaterest@users.noreply.github.com>
2022-02-17 10:14:30 -0500

    fix(note): key number (it's been wrong the whole time!)

--- src/note.rs
+++ src/note.rs
@@ -1,15 +1,16 @@
-const TONES: [u8; 11] = [
-    'a' as u8,
+const TONES: [u8; 12] = [
+    b'c',
     0,
-    'b' as u8,
-    'c' as u8,
+    b'd',
     0,
-    'd' as u8,
+    b'e',
+    b'f',
     0,
-    'e' as u8,
-    'f' as u8,
+    b'g',
     0,
-    'g' as u8
+    b'a',
+    0,
+    b'b',
 ];
 
 /// convert note to index (key number)
@@ -20,15 +21,15 @@ fn ntoi(n: &[u8]) -> u8 {
     // if flat or sharp
     if l == 3 {
         // +1 if sharp, -1 if flat
-        i = if n[2] == 'b' as u8 { i - 1 } else { i + 1 };
+        i = if n[1] == 'b' as u8 { i - 1 } else { i + 1 };
     }
     // https://en.wikipedia.org/wiki/Piano_key_frequencies
-    // 48 is '0'
-    i + 12 * (n[l - 1] - 48 - (i > 2) as u8)
+    // 48 is '0', so -49 for -1
+    i + 4 + 12 * (n[l - 1] - 49)
 }
 
 /// convert note to its frequency
 pub fn ntof(n: &[u8]) -> f64 {
-    // -48 because zero-index
-    2f64.powf((ntoi(n) as f64 - 48.0) / 12.0) * 440.0
+    // https://en.wikipedia.org/wiki/Piano_key_frequencies
+    2f64.powf((ntoi(n) as f64 - 49.0) / 12.0) * 440.0
 }
