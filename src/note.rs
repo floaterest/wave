4a813a5235ce5c8b60d746c79a5bce946bdcdb8c
Floaterest <floaterest@outlook.jp>
2022-05-05 08:11:21 -0400

    feat(note) assert if invalid note

--- src/note.rs
+++ src/note.rs
@@ -1,3 +1,5 @@
+use std::str::from_utf8;
+
 const TONES: [u8; 12] = [
     b'c',
     0,
@@ -12,6 +14,19 @@ const TONES: [u8; 12] = [
     0,
     b'b',
 ];
+
+/// check if is valid note
+fn valid(n: &[u8]) -> bool {
+    match n.len() {
+        // rest
+        1 if !n[0].is_ascii_alphanumeric() => true,
+        // natural
+        2 if b'a' <= n[0] && n[0] <= b'g' && n[1].is_ascii_digit() => true,
+        // sharp/flat
+        3 if b'a' <= n[0] && n[0] <= b'g' && (n[1] == b'b' || n[1] == b'#') && n[2].is_ascii_digit() => true,
+        _ => false,
+    }
+}
 
 /// convert note to index (key number)
 /// e.g. a3 to 36, db3 to 30, f4 to 44 (zero-indexed)
@@ -30,7 +45,8 @@ fn ntoi(n: &[u8]) -> u8 {
 
 /// convert note to its frequency
 pub fn ntof(n: &[u8]) -> f64 {
-    if n.len() > 0 && n[0].is_ascii_alphabetic() {
+    assert!(valid(n), "Invalid note: {}", from_utf8(n).unwrap());
+    if n[0].is_ascii_alphabetic() {
         // https://en.wikipedia.org/wiki/Piano_key_frequencies
         2f64.powf((ntoi(n) as f64 - 49.0) / 12.0) * 440.0
     } else {
