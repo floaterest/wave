514bde10ad9e157af6d811afde5973650df754e7
floaterest <56704092+floaterest@users.noreply.github.com>
2022-08-08 18:15:16 -0400

    refactor(*.rs) redefine line

--- src/parsers/parser.rs
+++ src/parsers/parser.rs
@@ -1,27 +1,26 @@
 use std::fs::File;
 use std::io::Result;
 
-use crate::buffer::Buffer;
+use crate::line::{Chord, Line};
 use crate::parsers::NoteParser;
 use crate::repeat::Repeat;
+use crate::waveform::Waveform;
 use crate::writer::Writer;
 
-pub const DOTTED: char = '.';
 pub const STACCATO: char = '*';
-pub const TIE: char = '+';
 pub const REPEAT: char = '|';
 
 pub struct Parser {
     writer: Writer,
     repeat: Repeat,
-    buffer: Buffer,
+    wave: Waveform,
     note: NoteParser,
 }
 
 impl Parser {
     pub fn new(dest: File, fps: u32, amp: f64) -> Self {
         Self {
-            buffer: Buffer::new(amp, fps),
+            wave: Waveform::new(amp, fps),
             writer: Writer::new(dest),
             repeat: Repeat::new(),
             note: NoteParser::new(),
@@ -29,21 +28,21 @@ impl Parser {
     }
     /// parse lines of input and write wave file
     pub fn write<I: Iterator<Item=String>>(&mut self, lines: I) -> Result<()> {
-        self.writer.start(self.buffer.fps)?;
+        self.writer.start(self.wave.fps)?;
         let lines = lines.map(|line| line.trim().to_string()).filter(|line| line.len() > 0);
         for line in lines {
-            self.parse_line(line)?;
+            self.parse(line)?;
         }
-        self.writer.write(self.buffer.drain(self.buffer.len()))?;
+        self.writer.write(self.wave.drain(self.wave.len()))?;
         Ok(self.writer.finish()?)
     }
     /// parse line as input
-    fn parse_line(&mut self, line: String) -> Result<()> {
+    fn parse(&mut self, line: String) -> Result<()> {
         Ok(match line.split_whitespace() {
             sw if line.contains(REPEAT) => self.parse_repeat(sw),
             sw if line.chars().next().unwrap().is_ascii_digit() => match line.parse::<u16>() {
-                Ok(bpm) => self.buffer.bpm = bpm,
-                Err(..) => self.parse_notes(sw)?,
+                Ok(bpm) => self.wave.bpm = bpm,
+                Err(..) => self.parse_line(sw)?,
             }
             _ => {},
         })
@@ -62,7 +61,7 @@ impl Parser {
             // end all voltas
             Some("") => self.repeat.clear(),
             Some(":") => {
-                self.repeat.repeat(&mut self.buffer, &mut self.writer);
+                self.repeat.repeat(&mut self.wave, &mut self.writer);
                 // if doesn't have voltas starting from 1
                 if self.repeat.voltas.len() == 1 { self.repeat.clear(); }
             }
@@ -81,52 +80,33 @@ impl Parser {
             _ => panic!("Invalid repeat start token: {}", token),
         }
     }
-    /// parse a line of input as a note
-    fn parse_notes<'a, I: Iterator<Item=&'a str>>(&mut self, tokens: I) -> Result<()> {
-        let (mut offset, mut len) = (0, 0);
-        let mut size = self.buffer.len();
-        tokens.for_each(|token: &str| match token.chars().next() {
-            // note length
-            Some(b) if b.is_ascii_digit() => {
-                len = self.parse_len(token);
-                // need more space for this len
-                if len > size {
-                    size = len;
-                    self.buffer.resize(size);
-                }
-                // always set offset as shortest len
-                if offset == 0 || len < offset { offset = len; }
-                // stacatto ==> half note length
-                if token.ends_with(STACCATO) { len /= 2; }
-            },
-            // note value
-            _ => {
-                let freq = self.note.frequency(token);
-                self.buffer.add(len, freq);
-                self.repeat.push(len, freq);
-            },
-        });
-        self.repeat.flush(size, offset);
-        Ok(self.writer.write(self.buffer.drain(offset))?)
+    fn bton(&self, beat: f64) -> usize {
+        //          duration in seconds        )     number of frames    )
+        ((beat * 240.0 / self.wave.bpm as f64) * self.wave.fps as f64) as usize
     }
-    /// parse a token as note length
-    /// returns number of frames
-    fn parse_len(&mut self, token: &str) -> usize {
-        fn strip(token: &str, suffix: char, scale: f64) -> f64 {
-            scale / token.strip_suffix(suffix).unwrap().parse::<f64>().unwrap()
-        }
-        let length = match token.parse::<usize>() {
-            Ok(value) => 1.0 / value as f64,
-            Err(..) => match token.chars().last() {
-                Some(DOTTED) => strip(token, DOTTED, 1.5),
-                // the actual node length will be parsed later
-                Some(STACCATO) => strip(token, STACCATO, 1.0),
-                _ if token.chars().all(
-                    |ch| ch.is_ascii_digit() || ch == TIE
-                ) => token.split(TIE).flat_map(|s| s.parse::<f64>()).map(|f| 1.0 / f).sum(),
-                _ => panic!("Unknown token as node length: {}", token)
+    fn parse_line<'a, I: Iterator<Item=&'a str>>(&mut self, tokens: I) -> Result<()> {
+        let mut line = Line::new();
+        let mut chord = Chord::new();
+        tokens.for_each(|token| match token.chars().next() {
+            // note length
+            Some(ch) if ch.is_ascii_digit() => {
+                if !chord.is_empty() {
+                    line.push(chord.clone());
+                    chord.clear();
+                }
+                let length = self.bton(Chord::parse_length(token));
+                chord.length = length;
+                chord.size = if token.ends_with(STACCATO) { length / 2 } else { length };
+            },
+            // note pitch
+            Some(ch) if ch.is_ascii_alphabetic() => {
+                chord.push(self.note.frequency(token));
             }
-        };
-        ((length * 240.0 / self.buffer.bpm as f64) * self.buffer.fps as f64) as usize
+            _ => panic!("Invalid token as line of chords: {}", token),
+        });
+        line.push(chord);
+        self.repeat.push(&line);
+        self.wave.add_l(&line);
+        Ok(self.writer.write(self.wave.drain(line.offset()))?)
     }
 }
--- src/repeat.rs
+++ src/repeat.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
-use crate::buffer::Buffer;
 use crate::line::Line;
+use crate::waveform::Waveform;
 use crate::writer::Writer;
 
 pub struct Repeat {
@@ -12,38 +12,25 @@ pub struct Repeat {
 
 impl Repeat {
     pub fn new() -> Self {
-        Self { voltas: vec![], current: 1, to_store: HashSet::new() }
+        Self { voltas: Vec::new(), current: 1, to_store: HashSet::new() }
     }
     // update set of voltas to store
     pub fn start(&mut self, indices: &[usize]) {
         match indices.iter().max() {
             Some(&size) => {
                 let size = size + 1;
-                if size > self.voltas.len() { self.voltas.resize(size, vec![]) }
+                if size > self.voltas.len() { self.voltas.resize(size, Vec::new()) }
                 // don't store current volta
                 self.to_store = indices.iter().filter(|&&i| i != self.current).cloned().collect();
-                let voltas = &mut self.voltas;
-                self.to_store.iter().for_each(|&i| voltas[i] = vec![Line::new()]);
             },
             None => panic!("Want to start repeat but indices are empty!")
         }
     }
     /// add a new note to the current voltas
-    pub fn push(&mut self, len: usize, freq: f64) {
+    pub fn push(&mut self, line: &Line) {
+        // todo let multiple v point to the same Vec of Lines
         for &v in self.to_store.iter() {
-            if let Some(line) = self.voltas[v].iter_mut().last() {
-                line.notes.push((len, freq));
-            }
-        }
-    }
-    /// add new line to each volta to store
-    pub fn flush(&mut self, size: usize, offset: usize) {
-        for &v in self.to_store.iter() {
-            if let Some(line) = self.voltas[v].iter_mut().last() {
-                line.size = size;
-                line.offset = offset;
-            }
-            self.voltas[v].push(Line::new());
+            self.voltas[v].push(line.clone());
         }
     }
     /// free all data
@@ -55,14 +42,14 @@ impl Repeat {
 
     //#region write
     /// write a volta
-    fn write(&mut self, v: usize, buffer: &mut Buffer, write: &mut dyn FnMut(Vec<i16>)) {
+    fn write(&mut self, v: usize, buffer: &mut Waveform, write: &mut dyn FnMut(Vec<i16>)) {
         for line in self.voltas[v].iter() {
-            buffer.add_line(line);
-            write(buffer.drain(line.offset));
+            buffer.add_l(line);
+            write(buffer.drain(line.offset()));
         }
     }
     /// repeat all needed voltas
-    pub fn repeat(&mut self, buffer: &mut Buffer, writer: &mut Writer) {
+    pub fn repeat(&mut self, buffer: &mut Waveform, writer: &mut Writer) {
         let mut write = |data| writer.write(data).unwrap();
         // append repeat
         self.write(0, buffer, &mut write);
